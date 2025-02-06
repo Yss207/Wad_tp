c64c386fc2f509760aa25ba9791cd232304c4477
Yash Sanjay Suryawanshi <133109961+Yss207@users.noreply.github.com>
2025-02-06 14:47:49 +0530

    Proper AJAX 1B Part

---
+++ users.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    let userList = $("#userList");
+
+    if(users.length === 0) {
+        userList.html(`<li>No users registered yet.</li>`);
+    } else {
+        users.forEach(user => {
+            userList.append(`<li>Name: ${user.name}, Email: ${user.email}, Number: ${user.number}</li>`);
+        });
+    }
+
+    $(document).ready(function(){
+        
+    
+        let users = JSON.parse(localStorage.getItem("users")) || [];
+        users.pop(user);
+        localStorage.setItem("users", JSON.stringify(users));
+    
+       
+    })
+});
